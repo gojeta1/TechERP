7d0ee9168f8500ca4c6646a2f12ca2690ff02cf0
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2024-01-11 17:58:48 -0300

    Create modalpessoas.component.ts

---
+++ src/app/components/modals/modalpessoas/modalpessoas.component.ts
@@ -0,0 +1,17 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+declare var M: any;
+
+@Component({
+  selector: 'app-modalpessoas',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modalpessoas.component.html',
+  styleUrl: './modalpessoas.component.scss'
+})
+export class ModalpessoasComponent {
+
+
+}
