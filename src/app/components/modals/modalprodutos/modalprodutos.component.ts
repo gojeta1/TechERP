3ab3f66d19a73267e686a958087db57dc50ece56
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2024-01-11 17:58:57 -0300

    Create modalprodutos.component.ts

---
+++ src/app/components/modals/modalprodutos/modalprodutos.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var M: any;
+
+
+@Component({
+  selector: 'app-modalprodutos',
+  standalone: true,
+  imports: [],
+  templateUrl: './modalprodutos.component.html',
+  styleUrl: './modalprodutos.component.scss'
+})
+export class ModalprodutosComponent implements OnInit{
+  ngOnInit(): void {
+      const elems = document.querySelectorAll('select')
+      const instance = M.FormSelect.init(elems)
+  }
+}
