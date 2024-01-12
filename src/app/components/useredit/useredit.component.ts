711823712a50270a9362a35f4e85ccf852c773f3
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2024-01-12 00:06:37 -0300

    Update useredit.component.ts

--- src/app/components/useredit/useredit.component.ts
+++ src/app/components/useredit/useredit.component.ts
@@ -1,61 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, Input} from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NavComponent } from '../nav/nav.component';
 import { Router } from '@angular/router';
+
 declare var M: any;
+
 
 @Component({
   selector: 'app-useredit',
   standalone: true,
   imports: [
     CommonModule,
-    NavComponent,
     FormsModule,
   ],
   templateUrl: './useredit.component.html',
   styleUrl: './useredit.component.scss'
 })
-export class UsereditComponent implements AfterViewInit {
+export class UsereditComponent {
 
-  dados: {
-    nome: string;
-    telephone: string;
-    place: string;
-    email: string;
-    cpf: number;
-    cidade: string;    
-  } | any
+
+    file: null | any
+    filepath: string = ''
+    nome: string  = ''
+    telephone: string = ''
+    place: string = ''
+    email: string = ''
+    cpf: string = ''
+    cidade: string = ''
+  
+
 
 
 
   constructor(private router: Router){}
 
-  ngAfterViewInit(): void {
-    const elems = document.querySelectorAll('select');
-    const instances = M.FormSelect.init(elems, {
-    });
-  }
-  
-  closeEdit(){
-      this.router.navigate(['/home/dashboard'])
-  }
-
   clearDados(){
-    if(this.dados === '' && this.dados.departamento === false){
-      M.toast({html: 'Não existe dados preenchidos', classes: 'red lighten-1'});
-    }else{
-      this.dados = { 
-        nome: '',
-        telephone: '',
-        place: '',
-        email: '',
-        cpf: '',
-        cidade: '',
-      };
 
-      // console.log(this.dados.departamento);
-    }
+      this.filepath = '',
+      this.nome = '',
+      this.telephone = '',
+      this.place = '',
+      this.email = '',
+      this.cpf = '',
+      this.cidade = ''
   }
 
   editedUser(){
