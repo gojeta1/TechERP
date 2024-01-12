08620268902d0d824723949d35d1f6a41420df9b
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2024-01-12 16:07:22 -0300

    Update usuarios.component.ts

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { ModalusuariosComponent } from '../modals/modalusuarios/modalusuarios.component';
+
+declare var M: any;
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ModalusuariosComponent],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.scss'
 })
 
-export class UsuariosComponent {
+export class UsuariosComponent implements OnInit{
+
+  constructor(){}
+
+  ngOnInit(): void {
+    const elems = document.querySelectorAll('.modal')
+    const instance = M.Modal.init(elems)
+  }
 
   email = {
     guilherme: 'guilherme@gmail.com',
