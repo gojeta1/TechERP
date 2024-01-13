ecc0f2f4df8b88fb8c0bc92ba54dd9e2985eb732
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2024-01-13 15:46:33 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from './auth/auth.guard';
-import { UsereditComponent } from './components/useredit/useredit.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PessoasComponent } from './components/pessoas/pessoas.component';
 import { ProdutosComponent } from './components/produtos/produtos.component';
-import { FinanceiroComponent } from './components/financeiro/financeiro.component';
-import { ContratosComponent } from './components/contratos/contratos.component';
 import { EstoqueComponent } from './components/estoque/estoque.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
-import { ConfigComponent } from './components/config/config.component';
 import { NotificationComponent } from './components/notification/notification.component';
 
 export const routes: Routes = [
@@ -32,11 +28,6 @@ export const routes: Routes = [
          component: DashboardComponent,
          canActivate: [AuthGuard],
          },
-         // {
-         //    path: 'useredit',
-         //    component: UsereditComponent,
-         //    canActivate: [AuthGuard],
-         // },
          {
             path: 'notification',
             component: NotificationComponent,
@@ -45,16 +36,6 @@ export const routes: Routes = [
          {
             path: 'pessoas',
             component: PessoasComponent,
-            canActivate: [AuthGuard],
-         },
-         {
-            path: 'financeiro',
-            component: FinanceiroComponent,
-            canActivate: [AuthGuard],
-         },
-         {
-            path: 'contratos',
-            component: ContratosComponent,
             canActivate: [AuthGuard],
          },
          {
@@ -72,11 +53,7 @@ export const routes: Routes = [
             component: UsuariosComponent,
             canActivate: [AuthGuard],
          },
-         {
-            path: 'config',
-            component: ConfigComponent,
-            canActivate: [AuthGuard],
-         },
+
        ],
      },
 
