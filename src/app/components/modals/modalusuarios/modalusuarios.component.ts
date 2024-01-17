a62ccc3d833a014e53e5ff1082282a1a4b8edc1c
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2024-01-17 18:11:14 -0300

    Update modalusuarios.component.ts

--- src/app/components/modals/modalusuarios/modalusuarios.component.ts
+++ src/app/components/modals/modalusuarios/modalusuarios.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, OnInit } from '@angular/core';
 
-declare var M:any;
+declare var M:any; 
 
 @Component({
   selector: 'app-modalusuarios',
@@ -15,65 +15,67 @@ export class ModalusuariosComponent implements OnInit{
   constructor(private el:ElementRef){}
 
   ngOnInit(): void {
-    const elems = this.el.nativeElement.querySelectorAll('select');
-    const instance = M.FormSelect.init(elems,{});
-
-    const elems2 = this.el.nativeElement.querySelectorAll('.datepicker');
-    const instance2 = M.Datepicker.init(elems2, {
-      i18n:{
-        months: [
-          'Janeiro',
-          'Fevereiro',
-          'Março',
-          'Abril',
-          'Maio',
-          'Junho',
-          'Julho',
-          'Agosto',
-          'Setembro',
-          'Outubro',
-          'Novembro',
-          'Dezembro'
-        ],
-        monthsShort: [
-          'Jan',
-          'Fev',
-          'Mar',
-          'Abr',
-          'Mai',
-          'Jun',
-          'Jul',
-          'Ago',
-          'Set',
-          'Out',
-          'Nov',
-          'Dez'
-        ],
-        weekdays: [
-          'Domingo',
-          'Segunda',
-          'Terça',
-          'Quarta',
-          'Quinta',
-          'Sexta',
-          'Sábado'
-        ],
-        weekdaysShort: [
-          'Dom',
-          'Seg',
-          'Ter',
-          'Qua',
-          'Qui',
-          'Sex',
-          'Sab'
-        ],
-        weekdaysAbbrev: [
-          'D', 'S','T','Q','Q','S','S'
-        ]
-      },
-      format: 'dd mmmm yyyy',
-      
-    });
+    if(typeof M !== 'undefined'){
+      const elems = this.el.nativeElement.querySelectorAll('select');
+      const instance = M.FormSelect.init(elems,{});
+  
+      const elems2 = this.el.nativeElement.querySelectorAll('.datepicker');
+      const instance2 = M.Datepicker.init(elems2, {
+        i18n:{
+          months: [
+            'Janeiro',
+            'Fevereiro',
+            'Março',
+            'Abril',
+            'Maio',
+            'Junho',
+            'Julho',
+            'Agosto',
+            'Setembro',
+            'Outubro',
+            'Novembro',
+            'Dezembro'
+          ],
+          monthsShort: [
+            'Jan',
+            'Fev',
+            'Mar',
+            'Abr',
+            'Mai',
+            'Jun',
+            'Jul',
+            'Ago',
+            'Set',
+            'Out',
+            'Nov',
+            'Dez'
+          ],
+          weekdays: [
+            'Domingo',
+            'Segunda',
+            'Terça',
+            'Quarta',
+            'Quinta',
+            'Sexta',
+            'Sábado'
+          ],
+          weekdaysShort: [
+            'Dom',
+            'Seg',
+            'Ter',
+            'Qua',
+            'Qui',
+            'Sex',
+            'Sab'
+          ],
+          weekdaysAbbrev: [
+            'D', 'S','T','Q','Q','S','S'
+          ]
+        },
+        format: 'dd mmmm yyyy',
+        
+      });  
+    }
   }
 
   private _avatarUrl: string | null = null;
